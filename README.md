273cdb5ab3f5f11d4816ea85ceb44f794e7dc3e7
Ulyana Klyuchnikova <ulyana.klyuchnikova@gmail.com>
2025-03-27 00:18:25 +0300

    with results

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Task Final (I made it as a branch from task 3 i know)
 
+### Nagging
+
 So first I would like to note that I have already finished first two punkts in my previous task. However this time I would like to properly add conda env!
 
 I tried to use a python file to build it. I put it in /envs folder. That file may have some inaccuracies, but basically I ran it, it created the config, then simply create env from config, then 
@@ -17,3 +19,18 @@ docker-compose -f docker-compose.yml up
 ```
 
 I tried some freaky dances with Dcoker file and making an image to update some gcc libs, but I STILL could not connect to env. I don't understad( I was forces to use pip install but at least I checked that it's all running
+
+I didn't really understand why that happened. I have mild suspicoun that on windows I got a wrong version of conda.
+
+### Results
+On test dataset (or rather a sample) we got these results. Seem pretty good - accuracy almost 0.9!
+That means everything works and I can go to bed
+
+FP16_Model:
+Classification Accuracy: 0.8924
+
+Prediction Precision: 0.9814271749755621
+
+Detection Recall: 0.8006379585326954
+
+Model F1: 0.8818620992534036
--- model_repository/resnet_preprocessing/1/model.py
+++ model_repository/resnet_preprocessing/1/model.py
@@ -3,7 +3,7 @@ import json
 import numpy as np
 import triton_python_backend_utils as pb_utils
 import hashlib
-from utils import preproc_images
+from utils import preprocess_image
 import shutil
 
 
@@ -16,19 +16,20 @@ class TritonPythonModel:
     def execute(self, requests):
         responses = []
         for request in requests:
-            image_filenames = [
-                text[0].decode("utf-8")
-                for text in pb_utils.get_input_tensor_by_name(
-                    request, "IMAGE_PATH"
-                ).as_numpy()
-            ]
+            image_data = pb_utils.get_input_tensor_by_name(
+                request, "IMAGE_DATA"
+            ).as_numpy()
 
-            preprocessed_images = preproc_images(image_filenames, target_size=224)
+            preprocessed_images = []
+            for img_array in image_data:
+                processed = preprocess_image(img_array)
+                preprocessed_images.append(processed)
 
-            preprocessed_images_np = np.array(preprocessed_images, dtype=np.float32)
+            preprocessed_images_np = np.stack(preprocessed_images)
             images_tensor_fp16 = pb_utils.Tensor(
                 self.fp16_output_name, preprocessed_images_np.astype(np.float16)
             )
-            response = pb_utils.InferenceResponse(output_tensors=[images_tensor_fp16])
-            responses.append(response)
+            responses.append(
+                pb_utils.InferenceResponse(output_tensors=[images_tensor_fp16])
+            )
         return responses
